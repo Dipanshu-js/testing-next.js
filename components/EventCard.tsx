45b305d3bef8a952261acd2290a933f55ad6b8e4
Dipanshu-js <officialvivi77@gmail.com>
2026-02-23 23:32:07 +0530

    EventDetailsPage

--- components/EventCard.tsx
+++ components/EventCard.tsx
@@ -24,7 +24,7 @@ const EventCard = ({ title, image, slug, location, date, time }: Props) => {
   };
 
   return (
-    <Link href={`/event`} id="event-card" onClick={handleClick}>
+    <Link href={`/events/${slug}`} id="event-card" onClick={handleClick}>
       <Image
         src={image}
         alt={title}
